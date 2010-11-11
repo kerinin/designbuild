7abc2504f2afb2316d53e775949888996c4c8bfa
Ryan Michael <kerinin@gmail.com>
2010-11-11 12:50:50 -0600

    passing derived quantity association tests

--- app/models/derived_quantity.rb
+++ app/models/derived_quantity.rb
@@ -2,6 +2,8 @@ class DerivedQuantity < ActiveRecord::Base
   belongs_to :component
   belongs_to :parent_quantity, :class_name => "Quantity"
   
+  has_many :unit_cost_estimates, :as => :quantity
+  
   validates_presence_of :parent_quantity
   
   before_save :set_component
--- app/models/quantity.rb
+++ app/models/quantity.rb
@@ -2,7 +2,7 @@ class Quantity < ActiveRecord::Base
   belongs_to :component
   
   has_many :derived_quantities, :foreign_key => :parent_quantity_id
-  has_many :unit_cost_estimates
+  has_many :unit_cost_estimates, :as => :quantity
   
   validates_presence_of :component
 end
