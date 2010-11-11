31448978c0269d1903cd6643705310fe6c46cb64
Ryan Michael <ryan@Jackie-Granellis-MacBook-Pro.local>
2010-11-11 12:43:00 -0600

    passing tests for quantity associations

--- app/models/quantity.rb
+++ app/models/quantity.rb
@@ -1,5 +1,8 @@
 class Quantity < ActiveRecord::Base
   belongs_to :component
   
+  has_many :derived_quantities, :foreign_key => :parent_quantity_id
+  has_many :unit_cost_estimates
+  
   validates_presence_of :component
 end
