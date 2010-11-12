45c8f5dc0bb1c445e9c31b265db19ecbd9c798a5
Ryan Michael <kerinin@gmail.com>
2010-11-11 19:12:18 -0600

    contract associations

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,3 +1,5 @@
 class Contract < ActiveRecord::Base
   has_many :tasks
+  has_many :costs, :class_name => "ContractCost"
+  has_many :bids
 end
--- test/unit/contract_test.rb
+++ test/unit/contract_test.rb
@@ -4,6 +4,11 @@ class ContractTest < ActiveSupport::TestCase
   context "A Contract" do
     setup do
       @obj = Factory :contract
+      
+      @c1 = Factory :contract_cost, :contract => @obj
+      @c2 = Factory :contract_cost, :contract => @obj
+      @b1 = Factory :bid, :contract => @obj
+      @b2 = Factory :bid, :contract => @obj
     end
 
     teardown do
@@ -17,6 +22,16 @@ class ContractTest < ActiveSupport::TestCase
     should "have values" do
       assert_not_nil @obj.contractor
       assert_not_nil @obj.bid
+    end
+    
+    should "allow multiple costs" do
+      assert_contains @obj.costs, @c1
+      assert_contains @obj.costs, @c2
+    end
+    
+    should "allow multiple bids" do
+      assert_contains @obj.bids, @b1
+      assert_contains @obj.bids, @b2
     end
   end
 end
