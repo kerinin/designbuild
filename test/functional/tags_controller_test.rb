13c19419bb98a9062bdff3bd2a53802611556387
Ryan Michael <ryan@bcarc.com>
2010-11-12 16:25:27 -0600

    fixing tags functional test

--- test/functional/tags_controller_test.rb
+++ test/functional/tags_controller_test.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class TagsControllerTest < ActionController::TestCase
   setup do
-    @tag = tags(:one)
+    @tag = Factory :tag
   end
 
   test "should get index" do
