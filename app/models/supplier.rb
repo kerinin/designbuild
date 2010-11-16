fc0aa536d6345162221edd3315b4e8b6497c73d2
Ryan Michael <ryan@bcarc.com>
2010-11-16 13:30:50 -0600

    added po and finished to suppliers

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,3 +1,13 @@
 class Supplier < ActiveRecord::Base
   has_many :material_costs
+  
+  validates_presence_of :name
+  
+  def purchase_orders
+    self.material_costs.where( :material_costs => {:cost => nil} )
+  end
+  
+  def completed_purchases
+    self.material_costs.where( "material_costs.cost IS NOT NULL" )
+  end
 end
--- test/unit/supplier_test.rb
+++ test/unit/supplier_test.rb
@@ -7,6 +7,8 @@ class SupplierTest < ActiveSupport::TestCase
       
       @mc1 = Factory :material_cost, :supplier => @obj
       @mc2 = Factory :material_cost, :supplier => @obj
+      @mc3 = Factory :material_cost, :supplier => @obj, :cost => nil
+      @mc4 = Factory :material_cost, :supplier => @obj, :cost => nil
     end
     
     teardown do
@@ -24,6 +26,20 @@ class SupplierTest < ActiveSupport::TestCase
     should "allow multiple material costs" do
       assert_contains @obj.material_costs, @mc1
       assert_contains @obj.material_costs, @mc2
+    end
+    
+    should "aggregate purchase orders" do
+      assert_contains @obj.purchase_orders.all, @mc3
+      assert_contains @obj.purchase_orders.all, @mc4
+      assert_does_not_contain @obj.purchase_orders.all, @mc1
+      assert_does_not_contain @obj.purchase_orders.all, @mc2
+    end
+    
+    should "aggregate completed purchases" do
+      assert_contains @obj.completed_purchases.all, @mc1
+      assert_contains @obj.completed_purchases.all, @mc2
+      assert_does_not_contain @obj.completed_purchases.all, @mc3
+      assert_does_not_contain @obj.completed_purchases.all, @mc4
     end
   end
 end
