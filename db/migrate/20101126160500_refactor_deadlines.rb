08e1713edee609f52944bb102b14f8b8befbc946
Ryan Michael <kerinin@gmail.com>
2010-11-26 10:25:18 -0600

    blocking out new deadline model

---
+++ db/migrate/20101126160500_refactor_deadlines.rb
@@ -0,0 +1,38 @@
+class RefactorDeadlines < ActiveRecord::Migration
+  class Deadline < ActiveRecord::Base
+  end
+  
+  class RelativeDeadline < ActiveRecord::Base
+  end
+  
+  def self.up
+    change_table :deadlines do |t|
+      t.integer :interval
+      
+      t.belongs_to :parent_deadline
+    end
+    
+    Deadline.reset_column_information
+    RelativeDeadline.all.each do {|rd| Deadline.create(rd.attributes) }
+    
+    drop_table :relative_deadlines
+  end
+
+  def self.down
+    create_table :relative_deadlines do |t|
+      t.string :name
+      t.integer :interval
+      
+      t.belongs_to :parent_deadline
+
+      t.timestamps
+    end
+    
+    RelativeDeadline.reset_column_information
+    Deadline.where(:date => nil).each do {|d| RelativeDeadline.create(d.attributes) }
+    
+    change_table :deadlines do |t|
+      t.remove :interval, :parent_deadline_id
+    end
+  end
+end
--- test/unit/deadline_test.rb
+++ test/unit/deadline_test.rb
@@ -3,12 +3,13 @@ require File.dirname(__FILE__) + '/../test_helper'
 class DeadlineTest < ActiveSupport::TestCase
   context "A Deadline" do
     setup do
-      @obj = Factory :deadline
+      @obj = Factory :deadline, :date => '1/1/2000'
+      @r_deadline = Factory :deadline, :parent_deadline => @obj, :interval => 5
       
       @t1 = Factory :task, :deadline => @obj
       @t2 = Factory :task, :deadline => @obj
-      @rd1 = Factory :relative_deadline, :parent_deadline => @obj
-      @rd2 = Factory :relative_deadline, :parent_deadline => @obj
+      @rd1 = Factory :deadline, :parent_deadline => @obj
+      @rd2 = Factory :deadline, :parent_deadline => @obj
     end
 
     teardown do
@@ -30,6 +31,22 @@ class DeadlineTest < ActiveSupport::TestCase
       end
     end
     
+    should "require an interval and parent if no date set" do
+      assert_raise ActiveRecord::RecordInvalid do
+        Factory :deadline, :parent_deadline => nil, :date => nil
+      end
+      
+      assert_raise ActiveRecord::RecordInvalid do
+        Factory :deadline, :interval => nil, :date => nil
+      end
+    end
+
+    should "be invalid if parent and date are both set" do
+      assert_raise ActiveRecord::RecordInvalid do
+        Factory :deadline, :parent_deadline => @obj, :date => '1/1/2000'
+      end
+    end
+        
     should "allow multiple tasks" do
       assert_contains @obj.tasks, @t1
       assert_contains @obj.tasks, @t2
@@ -38,6 +55,14 @@ class DeadlineTest < ActiveSupport::TestCase
     should "allow multiple relative deadlines" do
       assert_contains @obj.relative_deadlines, @rd1
       assert_contains @obj.relative_deadlines, @rd2
+    end
+    
+    should "allow a parent deadline" do
+      assert_equal @r_deadline.parent_deadline, @obj
+    end
+    
+    should "determine date" do
+      assert_equal @r_deadline.date, Date.parse('2000-1-6')
     end
   end
 end
