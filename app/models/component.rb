15de793cfbb59c805e3d58c49444b69694f09558
Ryan Michael <ryan@bcarc.com>
2010-12-01 11:46:18 -0600

    replacing acts_as_list (was failing at bc for some reason)

--- app/models/component.rb
+++ app/models/component.rb
@@ -16,7 +16,7 @@ class Component < ActiveRecord::Base
   
   has_and_belongs_to_many :tags
   
-  #acts_as_list :scope => :ancestry
+  acts_as_list :scope => :ancestry
   #acts_as_list :scope => :parent_id
   
   validates_presence_of :project, :name
