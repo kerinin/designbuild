8960fe0b78b29f3646b8e86ed41d11ce44cd8e51
Ryan Michael <kerinin@gmail.com>
2010-12-11 11:13:37 -0600

    updates to invoice line tests

--- db/migrate/20101211155214_add_project_retainage.rb
+++ db/migrate/20101211155214_add_project_retainage.rb
@@ -3,11 +3,13 @@ class AddProjectRetainage < ActiveRecord::Migration
     add_column :projects, :labor_percent_retainage, :float
     add_column :projects, :material_percent_retainage, :float
     add_column :projects, :contract_percent_retainage, :float
+    add_column :projects, :fixed_bid, :boolean, :default => false
   end
 
   def self.down
     remove_column :projects, :labor_percent_retainage
     remove_column :projects, :material_percent_retainage
     remove_column :projects, :contract_percent_retainage
+    remove_column :projects, :fixed_bid
   end
 end
--- test/unit/invoice_line_test.rb
+++ test/unit/invoice_line_test.rb
@@ -10,20 +10,20 @@ class InvoiceLineTest < ActiveSupport::TestCase
       # est: 100, cost: 50
       @contract = Factory :contract, :project => @project, :component => @component
       @bid = Factory :bid, :contract => @contract, :raw_cost => 100
-      @contract.update(:active_bid => @bid)
+      @contract.update_attributes(:active_bid => @bid)
       @contrat_invoice = Factory :contract_cost, :contract => @contract, :raw_cost => 50
       
       # est: 100, cost: 10, complete: 50
       @task1 = Factory :task, :project => @project
       @fce1 = Factory :fixed_cost_estimate, :raw_cost => 100, :task => @task1, :component => @component
       @lc1 = Factory :labor_cost, :task => @task1, :percent_complete => 50
-      @lcl1 = Factory :labor_cost_line, :labor_cost => @lc1, :laborer => @l, :hours => 10
+      @lcl1 = Factory :labor_cost_line, :labor_set => @lc1, :laborer => @l, :hours => 10
       
       # est: 1000, cost: 100, complete: 100
       @task2 = Factory :task, :project => @project      
       @fce2 = Factory :fixed_cost_estimate, :raw_cost => 1000, :task => @task2, :component => @component
       @lc2 = Factory :labor_cost, :task => @task2, :percent_complete =>100
-      @lcl2 = Factory :labor_cost_line, :labor_cost => @lc2, :laborer => @l, :hours => 100
+      @lcl2 = Factory :labor_cost_line, :labor_set => @lc2, :laborer => @l, :hours => 100
       
       # est: 10000, cost: 1000
       @task3 = Factory :task, :project => @project
@@ -31,30 +31,25 @@ class InvoiceLineTest < ActiveSupport::TestCase
       @mc1 = Factory :material_cost, :task => @task3, :raw_cost => 1000
       
       # requested: 2, paid: 1
-      @p_invoice = Factory :invoice, :project => @project, :date => Date::today - 10
+      @p_invoice = Factory :invoice, :project => @project, :date => Date::today - 10, :state => 'paid'
       @p_line = Factory( :invoice_line, 
         :invoice => @p_invoice, 
-        :labor_amount_requested => 5, 
-        :labor_amount_paid => 1, 
+        :labor_invoiced => 5, 
+        :labor_paid => 1, 
         :labor_retainage => 2, 
         :labor_retained => 1, 
-        :material_amount_requested => 50, 
-        :material_amount_paid => 10, 
+        :material_invoiced => 50, 
+        :material_paid => 10, 
         :material_retainage => 20, 
         :material_retained => 10,
-        :contract_amount_requested => 500, 
-        :contract_amount_paid => 100, 
+        :contract_invoiced => 500, 
+        :contract_paid => 100, 
         :contract_retainage => 200, 
-        :contract_retained => 100,
-        :state => 'paid'
+        :contract_retained => 100
       )
   
       @invoice = Factory :invoice, :project => @project
       @obj = Factory :invoice_line, :invoice => @invoice, :component => @component
-      
-      @fixed_invoice = Factory :invoice, :type => 'fixed_bid', @project => @project
-      @fixed_line = Factory :invoice_line, :invoice => @fixed_invoice, :component => @component
-      
     end
     
     should "be valid" do
@@ -127,23 +122,23 @@ class InvoiceLineTest < ActiveSupport::TestCase
       assert_equal 111, @component.retained
     end
         
-    should "default to cost - requested for cost+ component" do
+    should "default to cost - requested" do
       # this is interesting - should we include previously invoiced amounts that weren't paid?
       # I think not, otherwise we can't add up invoices to see how much we've invoiced total
       # better to include a line 'previously invoiced' to the view
 
       assert_equal (
-        ( @component.labor_cost * (1-@project.labor_percent_retainage_float ) )           # labor cost - retainage
-        + ( @component.material_cost * (1-@project.material_percent_retainage_float ) )   # material cost - retainage
-        + ( @component.contract_cost * (1-@project.contract_percent_retainage_float ) )   # contract cost - retainage
-        - @component.invoiced                                                             # invoiced to date (excluding retainage)
+        ( @component.labor_cost * (1-@project.labor_percent_retainage_float ) ) +          # labor cost - retainage
+        ( @component.material_cost * (1-@project.material_percent_retainage_float ) ) +  # material cost - retainage
+        ( @component.contract_cost * (1-@project.contract_percent_retainage_float ) ) -  # contract cost - retainage
+        @component.invoiced                                                             # invoiced to date (excluding retainage)
       ), @obj.invoiced
       
       assert_equal (
-        ( @component.labor_cost * (@project.labor_percent_retainage_float ) )           # labor retainage
-        + ( @component.material_cost * (@project.material_percent_retainage_float ) )   # material retainage
-        + ( @component.contract_cost * (@project.contract_percent_retainage_float ) )   # contract retainage
-        - @component.retainage                                                          # retainage to date
+        ( @component.labor_cost * (@project.labor_percent_retainage_float ) ) +          # labor retainage
+        ( @component.material_cost * (@project.material_percent_retainage_float ) ) +  # material retainage
+        ( @component.contract_cost * (@project.contract_percent_retainage_float ) ) -  # contract retainage
+        @component.retainage                                                          # retainage to date
       ), @obj.retainage
     end
     
@@ -152,8 +147,8 @@ class InvoiceLineTest < ActiveSupport::TestCase
       # note: this requires that task estimated cost + contract cost sums to component estimated cost
       
       assert_equal ( (
-        + (@task1.percent_complete_float * @task1.estimated_cost )              # task % of estimated cost
-        + (@task2.percent_complete_float * @task2.estimated_cost )
+        (@task1.percent_complete_float * @task1.estimated_cost ) +             # task % of estimated cost
+        (@task2.percent_complete_float * @task2.estimated_cost )
         ) / (@component.estimated_unit_cost + @component.estimated_fixed_cost)  # non-contract estimate
       ), @component.non_contract_percent_complete
       
@@ -168,32 +163,83 @@ class InvoiceLineTest < ActiveSupport::TestCase
       ), @component.percent_complete
     end
     
-    should "default to % complete * estimated - requested for fixed-bid component" do
-      assert_equal (
-        ( ( @component.non_contract_percent_complete * @component.estimated_labor_cost)       # percent of labor estimate
-          * (1-@project.labor_percent_retainage_float ) )                                     # labor retainage
-        + ( ( @component.non_contract_percent_complete * @component.estimated_meterial_cost)  # percent of material estimate
-          * (1-@project.material_percent_retainage_float ) )                                  # material retainage
-        + ( ( @component.contract_percent_complete * @component.estimated_contract_cost)      # percent of contract estimate
-          * (1-@project.contract_percent_retainage_float ) )                                  # contract retainage
-        - @component.invoiced                                                                 # invoiced to date (excluding retainage)
-      ), @obj.invoiced
-      
-      assert_equal (
-        ( ( @component.non_contract_percent_complete * @component.estimated_labor_cost)       # percent of labor estimate
-          * (@project.labor_percent_retainage_float ) )                                       # labor retainage
-        + ( ( @component.non_contract_percent_complete * @component.estimated_meterial_cost)  # percent of material estimate
-          * (@project.material_percent_retainage_float ) )                                    # material retainage
-        + ( ( @component.contract_percent_complete * @component.estimated_contract_cost)      # percent of contract estimate
-          * (@project.contract_percent_retainage_float ) )                                    # contract retainage
-        - @component.retainage                                                                # retainage to date
-      ), @obj.retainage
-    end
-    
     should "determine outstanding balance" do
       assert_equal (
         @obj.component.invoiced - @obj.component.paid
       ), @obj.outstanding
+    end
+  end
+  context "A Fixed-Bid Invoice Line" do
+    setup do
+      @l = Factory :laborer, :bill_rate => 1
+      @project = Factory :project, :fixed_bid => true, :labor_percent_retainage => 10, :material_percent_retainage => 20, :contract_percent_retainage => 30
+      @component = Factory :component, :project => @project
+      
+      # est: 100, cost: 50
+      @contract = Factory :contract, :project => @project, :component => @component
+      @bid = Factory :bid, :contract => @contract, :raw_cost => 100
+      @contract.update_attributes(:active_bid => @bid)
+      @contrat_invoice = Factory :contract_cost, :contract => @contract, :raw_cost => 50
+      
+      # est: 100, cost: 10, complete: 50
+      @task1 = Factory :task, :project => @project
+      @fce1 = Factory :fixed_cost_estimate, :raw_cost => 100, :task => @task1, :component => @component
+      @lc1 = Factory :labor_cost, :task => @task1, :percent_complete => 50
+      @lcl1 = Factory :labor_cost_line, :labor_set => @lc1, :laborer => @l, :hours => 10
+      
+      # est: 1000, cost: 100, complete: 100
+      @task2 = Factory :task, :project => @project      
+      @fce2 = Factory :fixed_cost_estimate, :raw_cost => 1000, :task => @task2, :component => @component
+      @lc2 = Factory :labor_cost, :task => @task2, :percent_complete =>100
+      @lcl2 = Factory :labor_cost_line, :labor_set => @lc2, :laborer => @l, :hours => 100
+      
+      # est: 10000, cost: 1000
+      @task3 = Factory :task, :project => @project
+      @fce3 = Factory :fixed_cost_estimate, :raw_cost => 10000, :task => @task3, :component => @component
+      @mc1 = Factory :material_cost, :task => @task3, :raw_cost => 1000
+      
+      # requested: 2, paid: 1
+      @p_invoice = Factory :invoice, :project => @project, :date => Date::today - 10, :state => 'paid'
+      @p_line = Factory( :invoice_line, 
+        :invoice => @p_invoice, 
+        :labor_invoiced => 5, 
+        :labor_paid => 1, 
+        :labor_retainage => 2, 
+        :labor_retained => 1, 
+        :material_invoiced => 50, 
+        :material_paid => 10, 
+        :material_retainage => 20, 
+        :material_retained => 10,
+        :contract_invoiced => 500, 
+        :contract_paid => 100, 
+        :contract_retainage => 200, 
+        :contract_retained => 100
+      )
+  
+      @invoice = Factory :invoice, :project => @project
+      @obj = Factory :invoice_line, :invoice => @invoice, :component => @component
+    end
+    
+    should "default to % complete * estimated - requested" do
+      assert_equal (
+        ( ( @component.non_contract_percent_complete * @component.estimated_labor_cost) *      # percent of labor estimate
+          (1-@project.labor_percent_retainage_float ) ) +                                    # labor retainage
+        ( ( @component.non_contract_percent_complete * @component.estimated_meterial_cost) * # percent of material estimate
+          (1-@project.material_percent_retainage_float ) ) +                                 # material retainage
+        ( ( @component.contract_percent_complete * @component.estimated_contract_cost) *     # percent of contract estimate
+          (1-@project.contract_percent_retainage_float ) ) -                                 # contract retainage
+        @component.invoiced                                                                 # invoiced to date (excluding retainage)
+      ), @obj.invoiced
+      
+      assert_equal (
+        ( ( @component.non_contract_percent_complete * @component.estimated_labor_cost) *      # percent of labor estimate
+          (@project.labor_percent_retainage_float ) ) +                                      # labor retainage
+        ( ( @component.non_contract_percent_complete * @component.estimated_meterial_cost) * # percent of material estimate
+          (@project.material_percent_retainage_float ) ) +                                   # material retainage
+        ( ( @component.contract_percent_complete * @component.estimated_contract_cost) *     # percent of contract estimate
+          (@project.contract_percent_retainage_float ) ) -                                   # contract retainage
+        @component.retainage                                                                # retainage to date
+      ), @obj.retainage
     end
   end
 end
