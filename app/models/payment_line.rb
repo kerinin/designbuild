6f9a0a055b00cb9d27881326543a115fec313c1b
Ryan Michael <kerinin@gmail.com>
2010-12-18 11:40:26 -0600

    payment line units

--- app/models/payment_line.rb
+++ app/models/payment_line.rb
@@ -22,10 +22,12 @@ class PaymentLine < ActiveRecord::Base
   protected
   
   def set_defaults
-    self.labor_paid ||= self.cost.labor_outstanding
-    self.material_paid ||= self.cost.material_outstanding
-    
-    self.labor_retained ||= self.cost.labor_retainage - self.cost.labor_retained
-    self.material_retained ||= self.cost.material_retainage - self.cost.material_retained
+    unless self.cost.nil?
+      self.labor_paid ||= self.cost.labor_outstanding
+      self.material_paid ||= self.cost.material_outstanding
+      
+      self.labor_retained ||= self.cost.labor_retainage - self.cost.labor_retained
+      self.material_retained ||= self.cost.material_retainage - self.cost.material_retained
+    end
   end
 end
