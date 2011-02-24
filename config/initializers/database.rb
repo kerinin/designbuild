46926b3b7faecc28c9062c8a49e0bd5652fb8f83
Ryan Michael <ryan@bcarc.com>
2011-02-24 12:07:07 -0600

    adding shared database env

--- config/initializers/database.rb
+++ config/initializers/database.rb
@@ -1,1 +1,2 @@
+ENV['SHARED_DATABASE_URL'] = 'postgres://scxmvoxqgi:0Y1uBK6cYzRtJmvXUhrq@ec2-75-101-167-19.compute-1.amazonaws.com/hmuppbvfij'
 ENV['DATABASE_URL'] = 'postgres://scxmvoxqgi:0Y1uBK6cYzRtJmvXUhrq@ec2-75-101-167-19.compute-1.amazonaws.com/hmuppbvfij'
