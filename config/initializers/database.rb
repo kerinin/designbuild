5ec2843b5e1b4c1d0610226fb4d9272790acf6ef
Ryan Michael <ryan@bcarc.com>
2011-02-24 12:17:08 -0600

    removing database auth

--- config/initializers/database.rb
+++
@@ -1,2 +0,0 @@
-ENV['SHARED_DATABASE_URL'] = 'postgres://scxmvoxqgi:0Y1uBK6cYzRtJmvXUhrq@ec2-75-101-167-19.compute-1.amazonaws.com/hmuppbvfij'
-ENV['DATABASE_URL'] = 'postgres://scxmvoxqgi:0Y1uBK6cYzRtJmvXUhrq@ec2-75-101-167-19.compute-1.amazonaws.com/hmuppbvfij'
