7a62fcab9779e0d9115d815346d2505f7e44158a
Ryan Michael <ryan@bcarc.com>
2011-06-15 16:30:01 -0500

    d&d gcal integration works locally

--- app/models/resource_allocation.rb
+++ app/models/resource_allocation.rb
@@ -9,12 +9,12 @@ class ResourceAllocation < ActiveRecord::Base
   validates_presence_of :start_date, :duration 
   validates_presence_of :resource_request, :unless => :nested
   
-  after_validation :on => :create do |r|
-    r.event_id = 'caching'
+  after_create do |r|
+    r.update_attributes :event_id => 'caching'
     r.delay.create_event
   end
   after_update do |r|
-    unless r.event_id.nil?
+    unless r.event_id.nil? || (r.event_id_changed? && ( r.event_id_was.nil? || r.event_id_was == 'caching') )
       r.delay.update_event 
     end
   end
@@ -22,7 +22,7 @@ class ResourceAllocation < ActiveRecord::Base
   before_save :get_resource
   after_destroy :update_request
   before_destroy do |r|
-    Delayed::Job.enqueue( DeleteEventJob.new(r.event_id) ) unless r.event_id.nil?
+    Delayed::Job.enqueue( DeleteEventJob.new(r.event_id) ) unless r.event_id.nil? || r.event_id == 'caching'
   end
   
   private
