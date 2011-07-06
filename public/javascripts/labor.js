27233befec5e1d313f6b9897a7c0b9d1e04af707
Ryan Michael <ryan@bcarc.com>
2011-07-06 17:10:31 -0500

    line item creating working

--- public/javascripts/labor.js
+++ public/javascripts/labor.js
@@ -1,6 +1,13 @@
 $(document).ready( function() {
-  $('ul.grouper > li.group_title').click(function(e){
+  $('body').delegate('ul.grouper > li.group_title', 'click', function(e) { 
     $(e.target).closest('.grouper li.group_title').next('li.group').slideToggle();
     $(e.target).closest('.grouper li.group_title').toggleClass('current');
-  })
-})
+  });
+  
+  $('body').delegate('li.add .label > a', 'click', function(e) {
+    $(e.target).closest('li.add .label').hide();
+    $(e.target).closest('li.add').find('form').slideToggle();
+    
+    return false;
+  });
+});
